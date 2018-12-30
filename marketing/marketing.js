2304fd11b1f8ce796b6ff2732df14961648fe0eb
Cameron Glass <glass10@purdue.edu>
2018-12-30 00:19:53 -0500

    Small fixes to Marketing

--- marketing/marketing.js
+++ marketing/marketing.js
@@ -41,52 +41,57 @@ function addMarketingHours(dateIndex){
 
     let start = startSelect.options[startSelect.selectedIndex].value;
     let end = endSelect.options[endSelect.selectedIndex].value;
-    
-    console.log("DATE: " + date + " START: " + start + " END: " + end);
 
-    let allOptions = [startSelect.options[0].value];
-    for(let i = 0; i < endSelect.length; i++){
-        allOptions.push(endSelect.options[i].value);
+    if(startSelect.selectedIndex > endSelect.selectedIndex){
+        alert("Please enter a correct time range");
     }
+    else{
+        console.log("DATE: " + date + " START: " + start + " END: " + end);
 
-    let allTimes = "";
-    let hours = 0;
-    for(let i = allOptions.indexOf(start); i < allOptions.indexOf(end); i++){
-        allTimes += (allOptions[i]) + ",";
-        hours += 0.5;
-    }
-    console.log(allTimes);
-
-    if (date === "" || start === "" || end === "") {
-        alert("Please provide values for all event details");
-    }
-    else {
-        var settings = {
-            "url": 'https://script.google.com/macros/s/AKfycbxTmGRbhyv56t3assM_urWIxMwGmnR82ltDsy_LnG0_oczCkQck/exec',/* TODO:  */
-            "type": "POST",
-            //"dataType": "json",
-            "data": {
-                "Member": currentName,
-                "Committee": currentCommittee,
-                "Date": date,
-                "Times": allTimes,
-                // "StartTime": start,
-                // "EndTime": end,
-                "Hours": hours
-            }
+        let allOptions = [startSelect.options[0].value];
+        for(let i = 0; i < endSelect.length; i++){
+            allOptions.push(endSelect.options[i].value);
         }
 
-        $.ajax(settings).done(function (response) {
-            alert("Marketing successfully added on " + date);
-            // Update 
-            viewScheduleForDate(0);
-            // Start Spinner
-            document.getElementById("topSpinner").style.visibility = "visible";
-        });
+        let allTimes = "";
+        let hours = 0;
+        for(let i = allOptions.indexOf(start); i < allOptions.indexOf(end); i++){
+            allTimes += (allOptions[i]) + ",";
+            hours += 0.5;
+        }
+        console.log(allTimes);
+
+        if (date === "" || start === "" || end === "") {
+            alert("Please provide values for all event details");
+        }
+        else {
+            var settings = {
+                "url": 'https://script.google.com/macros/s/AKfycbxTmGRbhyv56t3assM_urWIxMwGmnR82ltDsy_LnG0_oczCkQck/exec',/* TODO:  */
+                "type": "POST",
+                //"dataType": "json",
+                "data": {
+                    "Member": currentName,
+                    "Committee": currentCommittee,
+                    "Date": date,
+                    "Times": allTimes,
+                    // "StartTime": start,
+                    // "EndTime": end,
+                    "Hours": hours
+                }
+            }
+
+            $.ajax(settings).done(function (response) {
+                alert("Marketing successfully added on " + date);
+                // Update 
+                viewScheduleForDate(0);
+                // Start Spinner
+                document.getElementById("topSpinner").style.visibility = "visible";
+            });
+        }
     }
 }
 
-function getMembers(date, time){
+function getMembers(dateIndex){
     document.getElementById("topSpinner").style.visibility = "visible";
     var settings = {
         "async": true,
@@ -102,28 +107,34 @@ function getMembers(date, time){
     $.ajax(settings).done(function (response) {
         response = response.substring(response.indexOf("{"), response.length - 2);
         var response = JSON.parse(response);
-        
-        var dateChosen = date;
-        let allMembers = "";
-        for (var i = 0; i < response.feed.entry.length; i++) {
-            let data = response.feed.entry[i];
-            let name = data.gsx$member.$t;
-            let committee = data.gsx$committee.$t;
-            let date = data.gsx$date.$t;
-            let allTimes = data.gsx$times.$t;
-            // console.log("All Times: " + allTimes);
-            if(date === dateChosen){
-                if(allTimes.includes(time)){
-                    allMembers += name + ", ";
+
+        let dateChosen = allDates[dateIndex+locationIndex];
+        for(let j = 0; j < locationInfo[dateChosen].allTimes.length-1; j++){
+            let time = locationInfo[dateChosen].allTimes[j];
+            let allMembers = "";
+            for (var i = 0; i < response.feed.entry.length; i++) {
+                let data = response.feed.entry[i];
+                let name = data.gsx$member.$t;
+                let committee = data.gsx$committee.$t;
+                let date = data.gsx$date.$t;
+                let allTimes = data.gsx$times.$t;
+                // console.log("All Times: " + allTimes);
+                if(date === dateChosen){
+                    if(allTimes.includes(time) && !allMembers.includes(name)){
+                        allMembers += name + ", ";
+                    }
                 }
             }
+            // console.log(allMembers);
+            if(allMembers != ""){
+                document.getElementById(dateChosen+"-"+time).innerHTML = allMembers;
+                // document.getElementById("topSpinner").style.visibility = "hidden";
+            }
+            else{
+                document.getElementById(dateChosen+"-"+time).innerHTML = "Empty";
+            }
+            document.getElementById("topSpinner").style.visibility = "hidden";
         }
-        // console.log(allMembers);
-        if(allMembers != ""){
-            document.getElementById(dateChosen+"-"+time).innerHTML = allMembers;
-            // document.getElementById("topSpinner").style.visibility = "hidden";
-        }
-        document.getElementById("topSpinner").style.visibility = "hidden";
     });
 }
 
@@ -170,13 +181,13 @@ function viewScheduleForDate(direction){
                 memberTD.setAttribute("colspan", 2);
                 memberTD.setAttribute("id", dateInfo.date+"-"+dateInfo.allTimes[j]);
                 timeTD.innerHTML = dateInfo.allTimes[j];
-                memberTD.innerHTML = "Empty";
+                memberTD.innerHTML = `Loading...`;
 
                 newRow.appendChild(timeTD);
                 newRow.appendChild(memberTD);
                 if(j !== dateInfo.allTimes.length-1){
                     //Call to update
-                    getMembers(dateInfo.date, dateInfo.allTimes[j]);
+                    // getMembers(dateInfo.date, dateInfo.allTimes[j]);
 
                     // Append to table
                     document.getElementById("date-"+ (i-locationIndex) + "-sub-tab").appendChild(newRow);
