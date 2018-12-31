0b45179c0d645367177e5ffba28999fe76776ac6
Derek Lee <Derek@MacBook-Pro.local>
2018-12-30 18:02:23 -0800

    Added Tables, Load Members

--- attendance/attendance.js
+++ attendance/attendance.js
@@ -25,7 +25,8 @@ function load(){
         let firstName = currentName.substring(0, currentName.indexOf(" "));
         document.getElementById("navName").innerHTML = `Hi, ${firstName}!`;
         
-        loadAttendance();
+        console.log(currentCommittee);
+        loadMembers();
     }   
 }
 function loadAttendance() {
@@ -92,4 +93,72 @@ function loadAttendance() {
                     document.getElementById("tableBody").appendChild(tempTR);
             }
         });
+}
+
+function loadMembers() {
+    var committeeListed = ["artsAndCulture", "currentEvents", "entertainment", "publicity", "purdueAfterDark", "spiritAndTraditions"];
+        var committee = 0;
+        
+        console.log(currentCommittee);
+        for(var x = 0; x < committeeListed.length; x++) {
+            if(currentCommittee == committeeListed[x]) {
+                console.log("found committee %s", x+1);
+                committee = x+1;
+            }
+        }
+        console.log("commitee %s", committeeListed[committee-1]);
+        
+        
+        var sheetUrl = 'https://spreadsheets.google.com/feeds/cells/1xHh-igylkYXPXMEArgNJISWYgsJPMqo9ijUM72gmy58/' + committee + '/public/full?alt=json';
+        $.getJSON(sheetUrl, function(data){
+          var entry = data.feed.entry;
+          console.log(entry);
+            
+            var columns = entry[entry.length-1].gs$cell.col
+            console.log("columns: %s", columns);
+            
+            var membersArray = []; // array of events
+            var attendanceArray = []; // array of attendance
+            var row = 0; // row of found row
+                        
+            //parses member and its attendance values
+            for(var x = 0; x < entry.length; x++){
+                if(entry[x].gs$cell.col == "1"&& entry[x].gs$cell.row > 2) {
+                    membersArray.push(entry[x].content.$t);
+                }
+            }
+            console.log("members: %s", membersArray.toString()); // array of all attendance
+            console.log("row: %s", row); // row of current member
+            
+            var attendanceValues = ["Present", "Excused", "Unexcused"]
+            
+            for(var x = 0; x < membersArray.length; x++) {
+                let tempTR = document.createElement("tr");
+
+                    let members = document.createElement("td");
+                    members.innerHTML = membersArray[x];
+                
+                    let attendance = document.createElement("td");
+                
+                    let selectAttendance = document.createElement("select");
+                    selectAttendance.id = membersArray[x] + "dropdown";
+                    
+                    for(var y = 0; y < attendanceValues.length; y++) {
+                        var option = document.createElement("option");
+                        option.value = attendanceValues[y];
+                        option.text = attendanceValues[y];
+                    selectAttendance.appendChild(option);
+                    }
+
+                    attendance.appendChild(selectAttendance);
+                    tempTR.appendChild(members);
+                    tempTR.appendChild(attendance);
+
+                    document.getElementById("tableBody").appendChild(tempTR);
+            }
+        });
+}
+
+function submitAttendance(){
+    console.log("submit Attendance");
 }
